0cf7669b8baeba2d9d478e6389b19a9686eb7187
Motion <retonym@163.com>
2018-12-22 11:45:13 +0800

    Dec.22, 2018

---
+++ Q721AccountsMerge.py
@@ -0,0 +1,12 @@
+class Solution:
+    def accountsMerge(self, accounts):
+        """
+        :type accounts: List[List[str]]
+        :rtype: List[List[str]]
+        """
+
+
+if __name__ == '__main__':
+    print(Solution().accountsMerge([["John","johnsmith@mail.com","john_newyork@mail.com"],
+                                    ["John","johnsmith@mail.com","john00@mail.com"],
+                                    ["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]))
