9b6f1b62a60c82f770bd41e4284fd286ed3cbc88
Motion <retonym@163.com>
2019-01-19 20:44:51 +0800

    Jan.19, 2019

--- Q129SumRoottoLeafNumbers.py
+++ Q129SumRoottoLeafNumbers.py
@@ -1,1 +1,50 @@
-# TODO to be finished
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from Tree_Method import TreeNode, buildTree
+class Solution:
+    def __init__(self):
+        self._path_sum = 0
+
+    def sumNumbersRecur(self, root, prev_num):
+        cur_num = prev_num*10 + root.val
+        if root.left and root.right:
+            self.sumNumbersRecur(root.left, cur_num)
+            self.sumNumbersRecur(root.right, cur_num)
+        elif root.left:
+            self.sumNumbersRecur(root.left, cur_num)
+        elif root.right:
+            self.sumNumbersRecur(root.right, cur_num)
+        else:
+            self._path_sum += cur_num
+
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        cur_num = root.val
+
+        if root.left and root.right:
+            self.sumNumbersRecur(root.left, cur_num)
+            self.sumNumbersRecur(root.right, cur_num)
+        elif root.left:
+            self.sumNumbersRecur(root.left, cur_num)
+        elif root.right:
+            self.sumNumbersRecur(root.right, cur_num)
+        else:
+            self._path_sum += cur_num
+
+        return self._path_sum
+
+
+if __name__ == '__main__':
+    tree = buildTree([1,2,3])
+    print(Solution().sumNumbers(tree))
+
